0e33191732a9d21887034b19f436f0d013b2f59c
Khamel <khamel.mumford@gmail.com>
2018-02-01 23:45:19 -0600

    added and installed request and dotenv

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
-require("dotenv").config();
+const dotenv = require("dotenv").config("./.env");
+var request = require("request");
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+request(`https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2`, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(body);
+    }
+});
